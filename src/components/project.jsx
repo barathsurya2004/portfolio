eb41079021c40e0def6de6ddf5686f2a3dc30278
Zeus Crusher <barathsurya2004@gmail.com>
2024-01-01 21:46:51 +0530

    responsiveness added

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,17 +4,11 @@ import classes from './project.module.css';
 const Project = ({ imageUrl, title, desc, tech }) => {
     return (
         <div className={classes.container}>
-            <div style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "start"
-            }}>
-
-                <div className={classes.picture}>
-                    <div className={classes.hover} />
-                    <img src={imageUrl} alt="" />
-                </div>
+            <div className={classes.picture}>
+                <div className={classes.hover} />
+                <img src={imageUrl} alt="" />
             </div>
+
             <div className={classes.info}>
                 <h3>Featured Project</h3>
                 <h2>{title}</h2>
