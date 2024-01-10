bab4f2706e46cced2b816690c8cb65989d6d173f
Zeus Crusher <barathsurya2004@gmail.com>
2024-01-10 16:04:25 +0530

    projects responsiveness added

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,18 +4,18 @@ import classes from './project.module.css';
 const Project = ({ imageUrl, title, desc, tech }) => {
     return (
         <div className={classes.container}>
-            <div className={classes.picture}>
-                <div className={classes.hover} />
-                <img src={imageUrl} alt="" />
-            </div>
 
             <div className={classes.info}>
+                <div className={classes.picture}>
+                    <div className={classes.hover} />
+                    <img src={imageUrl} alt="" />
+                </div>
                 <h3>Featured Project</h3>
                 <h2>{title}</h2>
-                <div className={classes.desc}>{desc}.</div>
-                <div className={classes.tech}>
-                    {tech.map((ele) => <span>{ele} </span>)}
-
+                <div className={classes.desc}>{desc}.
+                    <div className={classes.tech}>
+                        {tech.map((ele) => <span>{ele} </span>)}
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/project2.jsx
+++ src/components/project2.jsx
@@ -7,20 +7,21 @@ const Project2 = ({ imageUrl, title, desc, tech }) => {
             <div className={classes.info}>
                 <h3>Featured Project</h3>
                 <h2>{title}</h2>
-                <div className={classes.desc}>{desc}.</div>
-                <div className={classes.tech}>
-                    {tech.map((ele) => <span>{ele} </span>)}
+                <div className={classes.desc}>{desc}.
+                    <div className={classes.tech}>
+                        {tech.map((ele) => <span>{ele} </span>)}
 
+                    </div>
                 </div>
-            </div>
-            <div style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "end"
-            }}>
-                <div className={classes.picture}>
-                    <div className={classes.hover} />
-                    <img src={imageUrl} alt="" />
+                <div style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "end"
+                }}>
+                    <div className={classes.picture}>
+                        <div className={classes.hover} />
+                        <img src={imageUrl} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
